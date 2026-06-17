316a74c94227af38132b26548586c11c3bec6534
yongzheluka <799447176@qq.com>
2026-06-17 20:18:11 +0800

    简单修改

--- src/config/announcementConfig.ts
+++ src/config/announcementConfig.ts
@@ -5,10 +5,10 @@ export const announcementConfig: AnnouncementConfig = {
 	title: "公告",
 
 	// 公告内容
-	content: "欢迎来到我的博客！这是一则示例公告。勇者鲁卡",
+	content: "欢迎来到我的博客！此博客仍在搭建中。——勇者鲁卡",
 
 	// 是否允许用户关闭公告
-	closable: true,
+	closable: false,
 
 	link: {
 		// 启用链接
--- src/config/pioConfig.ts
+++ src/config/pioConfig.ts
@@ -3,7 +3,7 @@ import type { Live2DWidgetConfig, SpineModelConfig } from "../types/pioConfig";
 // Spine 看板娘配置
 export const spineModelConfig: SpineModelConfig = {
 	// Spine 看板娘开关
-	enable: false,
+	enable: true,
 
 	// Spine模型配置
 	model: {
